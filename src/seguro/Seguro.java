f0cde491fc1e0c547cbc8e237c98d907544adb1e
Josek <josecarlobastos@gmail.com>
2020-02-22 15:05:20 -0500

    iniciando

---
+++ src/seguro/Seguro.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package seguro;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author INTEL POWER USER
+ */
+public class Seguro {
+    
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String fecha = "12/02/94";
+        String [] arr = fecha.split(fecha);
+        System.out.println(arr[2]);
+    }
+    
+}
